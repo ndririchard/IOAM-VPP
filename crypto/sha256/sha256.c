6ab4517af7cc6dab2cf6b7cdab16f545f56860e8
ndririchard <ndririchard@outlook.com>
2022-08-25 01:15:55 +0200

    fix bug with sha256

--- crypto/sha256/sha256.c
+++ crypto/sha256/sha256.c
@@ -1,5 +1,8 @@
-#include "./sha256.h"
+#include <stdlib.h>
+#include <memory.h>
+#include "sha256.h"
 
+/****************************** MACROS ******************************/
 #define ROTLEFT(a,b) (((a) << (b)) | ((a) >> (32-(b))))
 #define ROTRIGHT(a,b) (((a) >> (b)) | ((a) << (32-(b))))
 
@@ -10,7 +13,8 @@
 #define SIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
 #define SIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
 
-u32 k[64] = {
+/**************************** VARIABLES *****************************/
+static const WORD k[64] = {
 	0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
 	0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
 	0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
@@ -21,9 +25,10 @@ u32 k[64] = {
 	0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
 };
 
-void sha256_transform(SHA256_CTX *ctx, u32 data[])
+/*********************** FUNCTION DEFINITIONS ***********************/
+void sha256_transform(SHA256_CTX *ctx, const BYTE data[])
 {
-	u8 a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
+	WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
 
 	for (i = 0, j = 0; i < 16; ++i, j += 4)
 		m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
@@ -76,9 +81,9 @@ void sha256_init(SHA256_CTX *ctx)
 	ctx->state[7] = 0x5be0cd19;
 }
 
-void sha256_update(SHA256_CTX *ctx, u8 data[], u32 len)
+void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len)
 {
-	u32 i;
+	WORD i;
 
 	for (i = 0; i < len; ++i) {
 		ctx->data[ctx->datalen] = data[i];
@@ -91,9 +96,9 @@ void sha256_update(SHA256_CTX *ctx, u8 data[], u32 len)
 	}
 }
 
-void sha256_final(SHA256_CTX *ctx, u8 hash[])
+void sha256_final(SHA256_CTX *ctx, BYTE hash[])
 {
-	u32 i;
+	WORD i;
 
 	i = ctx->datalen;
 
--- crypto/sha256/sha256.h
+++ crypto/sha256/sha256.h
@@ -1,19 +1,25 @@
 #ifndef SHA256_H
 #define SHA256_H
 
-#include "../src/vppinfra"
-#define SHA256_BLOCK_SIZE 32
 
-typedef struct{
-    u8 data[64];
-    u32 datalen;
-    u32 bitlen;
-    u32 state[8];
+#include <stddef.h>
+
+
+#define SHA256_BLOCK_SIZE 32            // SHA256 outputs a 32 byte digest
+
+typedef unsigned char BYTE;             // 8-bit byte
+typedef unsigned int  WORD;             // 32-bit word, change to "long" for 16-bit machines
+
+typedef struct {
+	BYTE data[64];
+	WORD datalen;
+	unsigned long long bitlen;
+	WORD state[8];
 } SHA256_CTX;
 
+
 void sha256_init(SHA256_CTX *ctx);
-void sha256_update(SHA256_CTX *ctx, u8 data[], u32 len);
-void sha256_final(SHA256_CTX *ctx, u8 hash[]);
+void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len);
+void sha256_final(SHA256_CTX *ctx, BYTE hash[]);
 
-
-#endif
+#endif  
